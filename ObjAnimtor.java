49c539c1cd72dd9c99ed0bdbe5b230b61b9f5116
uestc_xsf <xsf_uestc_ncl@163.com>
2016-06-09 21:30:51 +0800

    objectAnimator 封装

---
+++ ObjAnimtor.java
@@ -0,0 +1,65 @@
+package xsf.customView.util;
+
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.view.View;
+
+
+public final class ObjAnimtor {
+    public static Animator add(final View view, float x) {
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(view,
+                "translationX", x, 0.0F);
+        objectAnimator.setDuration(1000L);
+        objectAnimator.addListener(new Animator.AnimatorListener() {
+            public void onAnimationCancel(Animator paramAnonymousAnimator) {
+            }
+
+            public void onAnimationEnd(Animator animator) {
+            }
+
+            public void onAnimationRepeat(Animator animator) {
+            }
+
+            public void onAnimationStart(Animator animator) {
+
+            }
+        });
+        return objectAnimator;
+    }
+
+    public static Animator down(View view, float y) {
+        ObjectAnimator localObjectAnimator = ObjectAnimator.ofFloat(view,
+                "translationY", 0.0F, y);
+        localObjectAnimator.setDuration(2000L);
+        return localObjectAnimator;
+    }
+
+    public static Animator up(View view, float y) {
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(view,
+                "translationY", 0.0F, y);
+        objectAnimator.setDuration(2000L);
+        return objectAnimator;
+    }
+
+    public static Animator up(View view, float fromY, float toY) {
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(view,
+                "translationY", fromY, toY);
+        objectAnimator.setDuration(2000L);
+        return objectAnimator;
+    }
+
+    public static Animator upAndVanish(View view, float y) {
+        AnimatorSet animatorSet = new AnimatorSet();
+        Animator[] arrayOfAnimator = new Animator[2];
+        float[] floats = new float[2];
+        floats[0] = y;
+        floats[1] = (3.0F * y);
+        arrayOfAnimator[0] = ObjectAnimator.ofFloat(view, "translationY",
+                floats);
+        arrayOfAnimator[1] = ObjectAnimator.ofFloat(view, "alpha", 1.0F, 0.0F);
+        animatorSet.playTogether(arrayOfAnimator);
+        animatorSet.setDuration(2000L);
+        return animatorSet;
+    }
+}
